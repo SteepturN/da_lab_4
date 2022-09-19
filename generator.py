222ad55b31dbc3fa84b006dbc25b8dcd6dd36368
admin <admin@gg>
2022-09-19 10:24:54 +0300

    another random generator

--- generator.py
+++ generator.py
@@ -15,38 +15,30 @@ if __name__ == "__main__":
     count_of_tests = int( sys.argv[2] )
     el = ""
     elements = []
-    variants = [ "exist", "random" ]
+    variants = [ "space", "digit", "digit", "digit", "digit", "newline" ]
+    action = ""
     random.seed()
     with open( sys.argv[ 1 ], 'w' ) as output_file:
-        for _ in range( random.randint( 0, 10 ) ):
-            if not random.choice( [ True, False ] ):
-                el = ""
-                for _ in range( random.randint( 0, 3 ) ):
-                    el += "{0}".format( random.randint(0, 9) )
-                random_num = random.randint( 0, 2**5 )
-                elements.append( el.zfill( random_num ) )
+        for _ in range( random.randint( 0, 10000 ) ):
+            if random.choice( [ True, False, False, False, False ] ):
+                action = random.choice( variants )
+                if action == "space":
+                    el = " "
+                elif action == "digit":
+                    el = "{0}".format( random.randint(0, 9) )
+                elif action == "newline":
+                    el = "\n"
             else:
-                el = "? "
-            for _ in range( random.randint( 0, 2**6 - 1 ) ):
-                el += " "
+                el = " ? "
             output_file.write( "{0}".format( el ) )
-        output_file.write( "\n" )
-        for _ in range( random.randint( 0, count_of_tests ** 2 ) ):
+            if action == "newline":
+                break
+        for _ in range( random.randint( 0, count_of_tests ** 4 ) ):
             action = random.choice( variants )
-            if action == "exist":
-                word = "{0}".format( random.choice( elements ) \
-                                     if not len( elements ) == 0 \
-                                     else random.randint( 0, 10 ) )
-                random_num = random.randint( 0, 2**5 )
-                word = word.zfill( random_num )
-            elif action == "random":
-                word = ""
-                for _ in range( random.randint( 0, 3 ) ):
-                    word += "{0}".format( random.randint(0, 9) )
-                random_num = random.randint( 0, 2**5 )
-                word = word.zfill( random_num )
-            for _ in range( random.randint( 0, 2**5 ) ):
-                word += " "
-            for _ in range( random.randint( 0, 2**5 ) ):
-                word += "\n"
-            output_file.write( "{0}".format( word ) )
+            if action == "space":
+                el = " "
+            elif action == "digit":
+                el = "{0}".format( random.randint(0, 9) )
+            elif action == "newline":
+                el = "\n"
+            output_file.write( "{0}".format( el ) )
